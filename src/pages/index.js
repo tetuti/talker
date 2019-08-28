218d6af3e4212e452c8fc15229a5b8fc5ac64d73
tetuti <larsarle@gmail.com>
2019-08-28 20:46:39 +0200

    try to resolve SSR issue

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,16 +8,18 @@ const initialState = []
 const reducer = (state, action) => {
   const { type, payload } = action
   switch (type) {
-    case 'ADD_WORD':
+    case "ADD_WORD":
       return [...state, payload]
-    case 'REMOVE_WORD':
+    case "REMOVE_WORD":
       const index = state.indexOf(payload)
       console.log({
         index: index,
-        payload: payload
+        payload: payload,
       })
-      return index !== -1 ? [...state.slice(0, index), ...state.slice(index + 1)] : state 
-    case 'CLEAR_WORDS':
+      return index !== -1
+        ? [...state.slice(0, index), ...state.slice(index + 1)]
+        : state
+    case "CLEAR_WORDS":
       return []
     default:
       return state
@@ -26,29 +28,49 @@ const reducer = (state, action) => {
 
 const IndexPage = () => {
   const [state, dispatch] = useReducer(reducer, initialState)
-  const [input, setInput] = useState('')
-  const { listen, listening, stop } = useSpeechRecognition({
+  const [input, setInput] = useState("")
+  const { listen, listening, stop, supported } = useSpeechRecognition({
     onResult: result => {
-      dispatch({type: 'REMOVE_WORD', payload: result})
-    }
+      dispatch({ type: "REMOVE_WORD", payload: result })
+    },
   })
 
-  const toggle = listening ? stop : () => listen({lang: 'sv-SE'})
+  const toggle = listening ? stop : () => listen({ lang: "sv-SE" })
 
   return (
-  <Layout>
-    <h1 onClick = {toggle}>{ !listening ? '⏺️' : '⏹️'}️</h1>
-    <input value = {input} onChange = {event => setInput(event.target.value)}/>
-    <button onClick = {() => dispatch({type: 'ADD_WORD', payload: input})}>Lägg till ord</button>
-    <button onClick = {() => dispatch({type: 'CLEAR_WORDS'})}>Rensa</button>
-    { state.length !== 0 && (
-      <>
-        <h1>Säg</h1>
-        {state.map((word, index) => <h2 key = {index}>{word}</h2>)}
-      </>
-    )}
-    
-  </Layout>
-)}
+    <Layout>
+      {!supported ? (
+        <p>
+          Oh no, it looks like your browser doesn&#39;t support Speech
+          Recognition.
+        </p>
+      ) : (
+        <>
+          <h1 onClick={toggle}>{!listening ? "⏺️" : "⏹️"}️</h1>
+          <input
+            value={input}
+            onChange={event => setInput(event.target.value)}
+          />
+          <button
+            onClick={() => dispatch({ type: "ADD_WORD", payload: input })}
+          >
+            Lägg till ord
+          </button>
+          <button onClick={() => dispatch({ type: "CLEAR_WORDS" })}>
+            Rensa
+          </button>
+          {state.length !== 0 && (
+            <>
+              <h1>Säg</h1>
+              {state.map((word, index) => (
+                <h2 key={index}>{word}</h2>
+              ))}
+            </>
+          )}
+        </>
+      )}
+    </Layout>
+  )
+}
 
 export default IndexPage
